cd64823bc877845a0d4166fd7a06433e0c390c71
Collin Smallegan <cmsmallegan@comsma.com>
2022-12-05 22:55:29 -0600

    Update email.go

--- internal/message/email.go
+++ internal/message/email.go
@@ -6,8 +6,7 @@ import (
 )
 
 func SendEmail(name, message, emailAddress string) {
-	client := postmark.NewClient("e9884e47-892c-4ce6-8283-9749f2a701be", "10cf34a6-00d7-4896-848d-2cc88626b544")
-
+	client := postmark.NewClient()
 	email := postmark.Email{
 		From:       "hello@comsma.com",
 		To:         "cmsmallegan@comsma.com",
